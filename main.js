5e698b4652de6b6783b9c55d0d2ba90238b41b94
Adriana Pugarito <85944220+AdrianaPugarito@users.noreply.github.com>
2022-07-03 20:09:58 +0000

    Substituindo while por for

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function tocaSom(idElementoAudio) {//função com parâmetro
+/*function tocaSom(idElementoAudio) {//função com parâmetro
   document.querySelector(idElementoAudio).play();
 }
 
@@ -20,6 +20,30 @@ while (contador < listaDeTeclas.length) {
   }
 
   contador = contador + 1;
+
+  console.log(contador);
+}*/
+
+function tocaSom(idElementoAudio) {//função com parâmetro
+  document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;//template string
+
+  console.log(idAudio);
+
+  tecla.onclick = function () { //função anônima 
+    tocaSom(idAudio);
+  }
+
+  //contador = contador + 1; subtituido por contador++
 
   console.log(contador);
 }
