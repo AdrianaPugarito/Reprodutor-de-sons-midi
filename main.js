170c80148514b3473fdc187e6bcb38cbac828192
Adriana Pugarito <85944220+AdrianaPugarito@users.noreply.github.com>
2022-07-03 18:02:33 +0000

    Trabalhando com lista-automatização de código

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function tocaSomPom() {
+/*function tocaSomPom() {
   document.querySelector('#som_tecla_pom').play();
 }
+document.querySelector('.tecla_pom').onclick = tocaSomPom; 
+para não repetir nosso codigo 9 vezes vamos a trabalhar com listas, automatizando elementos*/
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
